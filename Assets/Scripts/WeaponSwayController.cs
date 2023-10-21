386b83281aac7de9ab4d96c3062a6c0dc0cd5114
DiegoJohnsonYZ <98571062+DiegoJohnsonYZ@users.noreply.github.com>
2023-10-21 09:52:25 -0500

    new Weapon

---
+++ Assets/Scripts/WeaponSwayController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponSwayController : MonoBehaviour
+{
+    public PlayerInput playerInput;
+    [SerializeField] private float swayMultiplier;
+    [SerializeField] private float smooth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementInput = playerInput.actions["MoveCamera"].ReadValue<Vector2>();
+
+        float mouseX = movementInput.x*swayMultiplier;
+        float mouseY = movementInput.y*swayMultiplier;
+
+
+        Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.right);
+        Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
+
+        Quaternion targetRotation = rotationX * rotationY;
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
+
+    }
+}

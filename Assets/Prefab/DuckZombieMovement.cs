835873f2022be561cf04a39f97b69861b7e7c141
Miriya <miriya_23@hotmail.com>
2023-10-21 15:27:58 -0500

    SpawnPatoCaminan

---
+++ Assets/Prefab/DuckZombieMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckZombieMovement : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 3.0f;
+    private bool isOnGround = false;
+    private bool isChasingPlayer = false;
+
+    private void Start()
+    {
+        // Busca el objeto con la etiqueta "Player" y asigna su transform al jugador.
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+    private void Update()
+    {
+        // Verifica si el jugador existe.
+        if (player != null)
+        {
+            if (isOnGround && !isChasingPlayer)
+            {
+                // Comienza a perseguir al jugador cuando está en el suelo.
+                isChasingPlayer = true;
+            }
+
+            if (isChasingPlayer)
+            {
+                // Calcula la dirección hacia el jugador.
+                Vector3 moveDirection = (player.position - transform.position).normalized;
+
+                // Mueve al pato zombie en la dirección del jugador a la velocidad especificada.
+                transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
